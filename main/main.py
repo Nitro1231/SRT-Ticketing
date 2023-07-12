8b16d3b6b0b0e0b67d819541880c5bfc24cb7271
Nitro <nitrodev0@gmail.com>
2023-07-12 19:27:27 +0900

    Add an SRT train snipe script.

--- main/main.py
+++ main/main.py
@@ -1,95 +1,48 @@
-import requests
-from bs4 import BeautifulSoup
-
-PAGE_ID = 'TK0101010000'
-
-json_data = {
-    'dptRsStnCd': '0551',
-    'arvRsStnCd': '0506',
-    'stlbTrnClsfCd': '05',
-    'psgNum': 1,
-    'seatAttCd': '015',
-    'isRequest': 'Y',
-    'dptRsStnCdNm': '수서',
-    'arvRsStnCdNm': '서대구',
-    'dptDt': '20220627',
-    'dptTm': '000000',
-    'chtnDvCd': 1,
-    'psgInfoPerPrnb1': 1,
-    'psgInfoPerPrnb5': 0,
-    'psgInfoPerPrnb4': 0,
-    'psgInfoPerPrnb2': 0,
-    'psgInfoPerPrnb3': 0,
-    'locSeatAttCd1': '000',
-    'rqSeatAttCd1': '015',
-    'trnGpCd': 109
-}
+import time
+import json
+from SRT import SRT
+from windows_toasts import WindowsToaster, ToastText1, ToastDuration
 
 
-'''
-?runDt1=
-&dptDt1=
-&dptTm1=
-&trnNo1=
-&trnGpCd1=
-&dptRsStnCd1=
-&arvRsStnCd1=
-&dptStnRunOrdr1=
-&arvStnRunOrdr1=
-&seatAttCd1=
-&psrmClCd1=
-&index1=
-&scarNo1=
-&chtnDvCd=1&jrnySqno=001&mode=1&psgNum=&pageId=TK0101010000
-'''
+USER_INFO = './user_info.json'
+WINDOW_SEAT = True
 
 
-def search_ticket():
-    content = ''
-    with requests.post(f'https://etk.srail.kr/hpg/hra/01/selectScheduleList.do?pageId={PAGE_ID}', json=json_data, stream=True) as response:
-        for line in response.iter_lines():
-            print(line)
-
-def test():
-    req = requests.post(f'https://etk.srail.kr/hpg/hra/01/selectPassengerResearchList.do?pageId={PAGE_ID}', json=json_data)
-    print(req.content)
-    return req
-
-def search_ticket_a():
-    req = requests.post(f'https://etk.srail.kr/hpg/hra/01/selectScheduleList.do?pageId={PAGE_ID}', json=json_data)
-    print(req.raw)
-    return req
-
-def collect_info(content):
-    sp = BeautifulSoup(content, 'html.parser')
-    print(sp)
-    print(sp.find_all('legend'))
+def search_trains(srt: SRT, params: dict) -> list['SRTTrain']:
+    return srt.search_train(params['dep'], params['arr'], params['date'], params['time'], params['time_limit'], params['available_only'])
 
 
-#search_ticket()
-
-test()
-
-# req = search_ticket()
-
-
-# data = collect_info(req.content)
-# print(len(req.text))
+def notification():
+    wintoaster = WindowsToaster('SRT Sniper')
+    newToast = ToastText1()
+    newToast.SetBody('Train Found!')
+    newToast.SetDuration(ToastDuration(ToastDuration.Short))
+    wintoaster.show_toast(newToast)
 
 
-'''
-?runDt1=
-&dptDt1=
-&dptTm1=
-&trnNo1=
-&trnGpCd1=
-&dptRsStnCd1=
-&arvRsStnCd1=
-&dptStnRunOrdr1=
-&arvStnRunOrdr1=
-&seatAttCd1=
-&psrmClCd1=
-&index1=
-&scarNo1=
-&chtnDvCd=1&jrnySqno=001&mode=1&psgNum=&pageId=TK0101010000
-'''
+if __name__ == '__main__':
+    # Get user info from user_info.json
+    with open(USER_INFO, encoding='utf8') as fp:
+        user_info = json.load(fp)
+
+    login_info = user_info['login_info']
+    search_params = user_info['search_params']
+
+    srt = SRT(login_info['username'], login_info['password'], False)
+
+    reserved = False
+    while not reserved:
+        for params in search_params:
+            trains = srt.search_train(params['dep'], params['arr'], params['date'], params['time'], params['time_limit'], params['available_only'])
+            print(trains)
+
+            if len(trains) > 0:
+                print('Seat Avaiable!')
+                srt.login()
+                srt.reserve(trains[0], window_seat=True)
+                srt.logout()
+
+                while True:
+                    notification()
+                    time.sleep(10)
+            time.sleep(3)
